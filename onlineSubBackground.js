e1c07e10529e1329c733d38a996ac049db1608a0
Ivan <i.oliynichenko@gmail.com>
2023-04-04 00:33:55 -0400

    Add moving subtitles. Fix bugs.

--- onlineSubBackground.js
+++ onlineSubBackground.js
@@ -1,67 +1,93 @@
-chrome.storage.sync.get('onlineSub').then(function(val){
-  if(val.onlineSub === undefined){
-    chrome.storage.sync.set({'onlineSub': true});
+chrome.runtime.onInstalled.addListener(details => {
+  if (details.reason === chrome.runtime.OnInstalledReason.INSTALL) {
+    chrome.storage.sync.set({ 'onlineSub': true }).then(() => {
+      injectOnlineSub();
+    })
   }
 });
 
-chrome.runtime.onMessage.addListener(function(message,sender,sendResponse){
-  if(message.onlineSubOffOn  === 'onlineSubOff'){
-    onlineSubOffOn('onlineSubOff')
-  }else if(message.onlineSubOffOn === 'onlineSubOn'){
-    onlineSubOffOn('onlineSubOn') 
-  }else if(message.onlineSubOnOnDomen === 'onlineSubOn'){
-    onlineSubOffOnDomen('onlineSubOn')
-  }else if(message.onlineSubOffOnDomen === 'onlineSubOff'){
-    onlineSubOffOnDomen('onlineSubOff')
-  }else if(message.changeGlobalSettings !== undefined){
-    changeGlobalSettings(message.changeGlobalSettings,sender.tab.id)
-  }else{
-    console.log('onlineSub backgroundscript message error')
-  }
-});
-
-function changeGlobalSettings(val,sender){
-  chrome.windows.getAll({populate:true},function(windows){
-    windows.forEach(function(window){
-      window.tabs.forEach(function(tab){
-        if(tab.id != sender)
-          chrome.tabs.sendMessage(tab.id, {'changeGlobalSettings': val });
+function injectOnlineSub() {
+  chrome.windows.getAll({ populate: true }, function (windows) {
+    windows.forEach(function (window) {
+      window.tabs.forEach(function (tab) {
+        if (tab.url.slice(0, 6) !== 'chrome' && tab.status === 'complete') {
+          chrome.scripting.executeScript({
+            target: { tabId: tab.id, allFrames: true },
+            files: ['onlineSubcontent.js']
+          });
+          chrome.scripting.insertCSS({
+            target: { tabId: tab.id, allFrames: true },
+            files: ['onlineSub.css']
+          });
+        }
       });
     });
   });
 }
 
-function onlineSubOffOn(val){
-  chrome.windows.getAll({populate:true},function(windows){
-    windows.forEach(function(window){
-      window.tabs.forEach(function(tab){
-        chrome.tabs.sendMessage(tab.id, {'onlineSub': val });
+chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+  if (message.onlineSubOffOn === 'onlineSubOff') {
+    onlineSubOffOn('onlineSubOff')
+  } else if (message.onlineSubOffOn === 'onlineSubOn') {
+    onlineSubOffOn('onlineSubOn')
+  } else if (message.onlineSubOnOnDomen === 'onlineSubOn') {
+    onlineSubOffOnDomen('onlineSubOn')
+  } else if (message.onlineSubOffOnDomen === 'onlineSubOff') {
+    onlineSubOffOnDomen('onlineSubOff')
+  } else if (message.changeGlobalSettings !== undefined) {
+    changeGlobalSettings(message.changeGlobalSettings, sender.tab.id)
+  } else {
+    console.log('onlineSub backgroundscript message error')
+  }
+});
+
+function changeGlobalSettings(val, sender) {
+  chrome.windows.getAll({ populate: true }, function (windows) {
+    windows.forEach(function (window) {
+      window.tabs.forEach(function (tab) {
+        if (tab.url.slice(0, 6) !== 'chrome' && tab.status === 'complete')
+          chrome.tabs.sendMessage(tab.id, { 'changeGlobalSettings': val }).catch(x => { });
+      });
+    });
+  });
+}
+
+function onlineSubOffOn(val) {
+  chrome.windows.getAll({ populate: true }, function (windows) {
+    windows.forEach(function (window) {
+      window.tabs.forEach(function (tab) {
+        if (tab.url.slice(0, 6) !== 'chrome' && tab.status === 'complete') {
+          chrome.tabs.sendMessage(tab.id, { 'onlineSub': val }).catch(x => { });
+        }
       });
     });
   });
 }
 
 function extractDomain(url) {
-    var domain;
-    if (url.indexOf("://") > -1) {
-        domain = url.split('/')[2];
-    }
-    else {
-        domain = url.split('/')[0];
-    }
-    domain = domain.split(':')[0];
-    return domain;
+  var domain;
+  if (url.indexOf("://") > -1) {
+    domain = url.split('/')[2];
+  }
+  else {
+    domain = url.split('/')[0];
+  }
+  domain = domain.split(':')[0];
+  return domain;
 }
 
-function onlineSubOffOnDomen(val){
-  var curTabUrl;
-  chrome.tabs.query({'active': true}, function (tabs) {
-   curTabUrl = extractDomain(tabs[0].url)
-  });
-  chrome.tabs.query({}, function (tabs) {
-   for(var i=0;i<tabs.length;i++){
-      if(tabs[i].url.slice(0,6) !== 'chrome' && extractDomain(tabs[i].url) === curTabUrl)
-        chrome.tabs.sendMessage(tabs[i].id, {'onlineSub': val });
-   }
-  });
+function onlineSubOffOnDomen(val) {
+  chrome.windows.getCurrent({ populate: true })
+    .then(window => {
+      return extractDomain(window.tabs.find(tab => tab.active).url)
+    })
+    .then(curTabUrl => {
+      chrome.tabs.query({}).then(tabs => {
+        tabs.forEach(tab => {
+          if (tab.url.slice(0, 6) !== 'chrome' && extractDomain(tab.url) === curTabUrl) {
+            chrome.tabs.sendMessage(tab.id, { 'onlineSub': val }).catch(x => { });
+          }
+        })
+      })
+    });
 }
--- onlineSubTop.js
+++ onlineSubTop.js
@@ -1,99 +1,101 @@
 var onoffOnlineSub = document.getElementById('onoffOnlineSub');
 var onoffOnlineSubOnPage = document.getElementById('onoffOnlineSubOnPage');
 var tabUrl;
+
 function extractDomain(url) {
-    var domain;
-    if (url.indexOf("://") > -1) {
-        domain = url.split('/')[2];
-    }
-    else {
-        domain = url.split('/')[0];
-    }
-    domain = domain.split(':')[0];
-    return domain;
+  var domain;
+  if (url.indexOf("://") > -1) {
+    domain = url.split('/')[2];
+  }
+  else {
+    domain = url.split('/')[0];
+  }
+  domain = domain.split(':')[0];
+  return domain;
 }
 
-chrome.tabs.query({'active': true}, function (tabs) {
-  tabUrl = extractDomain(tabs[0].url);
-});
 
-chrome.storage.sync.get('onlineSub').then(function(val) {
-  if(val.onlineSub === undefined){
+chrome.windows.getCurrent({ populate: true }).then(window => {
+  tabUrl = extractDomain(window.tabs.find(tab => tab.active).url)
+})
+
+chrome.storage.sync.get('onlineSub').then(function (val) {
+  if (val.onlineSub === undefined) {
     onoffOnlineSub.checked = true;
-  }else{
-    onoffOnlineSub.checked = val.onlineSub; 
+  } else {
+    onoffOnlineSub.checked = val.onlineSub;
   }
 });
 
-chrome.storage.sync.get('onlineSubOnPage').then(function(val) {
- onoffOnlineSubOnPage.checked = true;
-  if(val.onlineSubOnPage !== undefined){
-    for(var i=0;i<val.onlineSubOnPage.length;i++){
-      if(val.onlineSubOnPage[i] === tabUrl){
+chrome.storage.sync.get('onlineSubOnPage').then(function (val) {
+  onoffOnlineSubOnPage.checked = true;
+  if (val.onlineSubOnPage !== undefined) {
+    for (var i = 0; i < val.onlineSubOnPage.length; i++) {
+      if (val.onlineSubOnPage[i] === tabUrl) {
         onoffOnlineSubOnPage.checked = false;
       }
-    }  
+    }
   }
-}); 
+});
 
-onoffOnlineSubOnPage.addEventListener('click', onoffOnlineSubOnPageFunc, false);
-onoffOnlineSub.addEventListener('click', onoffOnlineSubFunc, false);
+onoffOnlineSubOnPage.addEventListener('change', onoffOnlineSubOnPageFunc, false);
+onoffOnlineSub.addEventListener('change', onoffOnlineSubFunc, false);
 
-function onoffOnlineSubOnPageFunc(){
-  
-  if(this.checked){
-    chrome.storage.sync.get('onlineSubOnPage').then(function(val) {
+function onoffOnlineSubOnPageFunc() {
+
+  if (this.checked) {
+    chrome.storage.sync.get('onlineSubOnPage').then(function (val) {
       var onlineSubOnPage;
-      if(val.onlineSubOnPage !== undefined){
+      if (val.onlineSubOnPage !== undefined) {
         onlineSubOnPage = val.onlineSubOnPage.indexOf(tabUrl)
-        if(onlineSubOnPage !== undefined){
+        if (onlineSubOnPage !== undefined) {
           val.onlineSubOnPage.splice(onlineSubOnPage, 1)
           onlineSubOnPage = val.onlineSubOnPage;
-          chrome.storage.sync.set({'onlineSubOnPage': onlineSubOnPage}).then(function(){
+          chrome.storage.sync.set({ 'onlineSubOnPage': onlineSubOnPage }).then(function () {
             onoffOnlineSubOnPage.checked = true;
             onlineSubOnOnDomen();
           });
         }
       }
     });
-  }else{
-    chrome.storage.sync.get('onlineSubOnPage').then(function(val) {
+  } else {
+    chrome.storage.sync.get('onlineSubOnPage').then(function (val) {
       var onlineSubOnPage = [];
-      
-      if(val.onlineSubOnPage === undefined){
+
+      if (val.onlineSubOnPage === undefined) {
         onlineSubOnPage.push(tabUrl);
-      }else{
+      } else {
         onlineSubOnPage = val.onlineSubOnPage.concat(tabUrl);
       }
-      chrome.storage.sync.set({'onlineSubOnPage': onlineSubOnPage}).then(function(){
+      chrome.storage.sync.set({ 'onlineSubOnPage': onlineSubOnPage }).then(function () {
         onoffOnlineSubOnPage.checked = false;
         onlineSubOffOnDomen();
       });
-    }); 
+    });
   }
 }
-function onoffOnlineSubFunc(){
-  if(this.checked){
-    chrome.storage.sync.set({'onlineSub': true}).then(function(){
+function onoffOnlineSubFunc() {
+  if (this.checked) {
+    chrome.storage.sync.set({ 'onlineSub': true }).then(function () {
       onlineSubOn();
       onoffOnlineSub.checked = true;
-    });  
-  }else{
-    chrome.storage.sync.set({'onlineSub': false}).then(function(){
+    });
+  } else {
+    chrome.storage.sync.set({ 'onlineSub': false }).then(function () {
       onlineSubOff();
       onoffOnlineSub.checked = false;
-    });    
+    });
   }
 }
-function onlineSubOff(){
-  chrome.runtime.sendMessage({'onlineSubOffOn': 'onlineSubOff'});
+function onlineSubOff() {
+  chrome.runtime.sendMessage({ 'onlineSubOffOn': 'onlineSubOff' });
 }
-function onlineSubOn(){
-  chrome.runtime.sendMessage({'onlineSubOffOn': 'onlineSubOn'});
+function onlineSubOn() {
+  chrome.runtime.sendMessage({ 'onlineSubOffOn': 'onlineSubOn' });
 }
-function onlineSubOffOnDomen(){
-  chrome.runtime.sendMessage({'onlineSubOffOnDomen': 'onlineSubOff'});
+function onlineSubOffOnDomen() {
+  chrome.runtime.sendMessage({ 'onlineSubOffOnDomen': 'onlineSubOff' });
 }
-function onlineSubOnOnDomen(){
-  chrome.runtime.sendMessage({'onlineSubOnOnDomen': 'onlineSubOn'});
+function onlineSubOnOnDomen() {
+  chrome.runtime.sendMessage({ 'onlineSubOnOnDomen': 'onlineSubOn' });
 }
